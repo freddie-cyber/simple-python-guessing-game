d4ba2313e9788e09a18b6dcf1c132e023e06667e
freddie-cyber <89332820+freddie-cyber@users.noreply.github.com>
2021-09-23 16:16:59 +1200

    Add files via upload

---
+++ simplepythonguessinggame.py
@@ -0,0 +1,18 @@
+print("hi this is a simple python guessing game")
+print("guess a number between 1-10")
+print("HINT 4-10")
+answer = input("answer:")
+
+if answer == "7":
+    print("correct")
+else:
+    print("incorrect")
+print("here is are more advanced question")
+print("guess my number between 1-20")
+print("HINT 14-20")
+correct = input("answer:")
+
+if correct == "18": 
+     print("correct")
+else:
+    print("incorrect")
